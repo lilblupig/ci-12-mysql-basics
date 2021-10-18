7d5ef6f7bc8e5d0ce02ef6a03b17d81171f76d08
lilblupig <lilblupig@gmail.com>
2021-10-18 19:45:42 +0000

    Add a new table, and insert content

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 # Get username from Gitpod
@@ -6,17 +7,16 @@ username = os.getenv('C9_USER')
 
 # Connect to database
 connection = pymysql.connect(host='localhost',
-                            user = username,
+                            user=username,
                             password='',
                             db='Chinook')
 
 try:
     #Run a query
     with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-        sql = "SELECT * FROM Genre;"
-        cursor.execute(sql)
-        for row in cursor:
-            print(row)
+        row = ("Bob", 21, "1990-02-06 23:04:56")
+        cursor.execute("INSERT INTO Friends VALUES(%s, %s, %s);", row)
+        connection.commit()
 finally:
     # Close connection, whether or not successful
     connection.close()
